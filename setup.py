a2b35db90bac46c8ddc36315e11530ac8e80098c
Ronen Artzi <ronen.artzi@gmail.com>
2021-04-02 19:23:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup_args = dict(
     name='fantastic_ascii',
-    version='1.0.1',
+    version='1.0.2',
     description='Fantastic ASCII',
     license='MIT',
     packages=find_packages(),
